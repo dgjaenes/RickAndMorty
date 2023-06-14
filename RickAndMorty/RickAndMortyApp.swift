76d03777020e45c529cfd5fe2d344cd5494f313f
David GJ <dgonz192@corp.vodafone.es>
2023-06-14 12:27:25 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by NEORIS on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

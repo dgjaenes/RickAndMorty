12cda0c7e30226b7028ddcdd1f8c0bced36efc7a
David GJ <dgonz192@corp.vodafone.es>
2023-06-15 13:46:46 +0200

    add cache request

--- RickAndMorty/Domain/Repository/ManagerRAMAppRepository.swift
+++ RickAndMorty/Domain/Repository/ManagerRAMAppRepository.swift
@@ -9,23 +9,32 @@ import Combine
 class ManagerRAMAppRepository {
     
     private let session: URLSession
+    let cache = NSCache<AnyObject, AnyObject>()
     
     init(session: URLSession = .shared) {
         self.session = session
+        cache.countLimit = 50
     }
     
-    internal func execute<T>(components: URLComponents) -> AnyPublisher<T, RAMAppError> where T: Decodable {
+    internal func execute<T>(components: URLComponents, inCacheValid: Bool = true) -> AnyPublisher<T, RAMAppError> where T: Decodable {
         guard let url = components.url else {
             let error = RAMAppError.network(description: "Couldn't create URL")
             return Fail(error: error).eraseToAnyPublisher()
         }
+
+        if inCacheValid, let cached = cache.object(forKey: url as AnyObject) as? T {
+            return Just(cached)
+                .setFailureType(to: RAMAppError.self)
+                .eraseToAnyPublisher()
+        }
+
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
+
         return session.dataTaskPublisher(for: request)
             .mapError { error in
-            .network(description: error.localizedDescription)
+                .network(description: error.localizedDescription)
             }
             .print()
             .map { data, response  in
@@ -42,6 +51,9 @@ class ManagerRAMAppRepository {
             .flatMap() { item in
                 self.decode(item)
             }
+            .handleEvents(receiveOutput: { [weak self] decoded in
+                self?.cache.setObject(decoded as AnyObject, forKey: url as AnyObject)
+            })
             .eraseToAnyPublisher()
     }
     
